859655f2aa71938e1328438d5c964a4f031660c7
dawidj <dawidjanicki2@gmail.com>
2023-12-15 11:39:11 +0100

    Add OrdersList

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+export type OrderType = {
+  id: string;
+  clientName: string;
+  orderNumber: number;
+  startDate: Date;
+  endDate: Date;
+  isFinished: boolean;
+  size: number;
+};
+
+const sampleOrders: OrderType[] = [
+  {
+    id: "1",
+    clientName: "Alice",
+    orderNumber: 101,
+    startDate: new Date("2023-01-01"),
+    endDate: new Date("2023-01-10"),
+    isFinished: false,
+    size: 5,
+  },
+  {
+    id: "2",
+    clientName: "Bob",
+    orderNumber: 102,
+    startDate: new Date("2023-02-01"),
+    endDate: new Date("2023-02-10"),
+    isFinished: true,
+    size: 3,
+  },
+  {
+    id: "3",
+    clientName: "Charlie",
+    orderNumber: 103,
+    startDate: new Date("2023-03-01"),
+    endDate: new Date("2023-03-10"),
+    isFinished: false,
+    size: 4,
+  },
+  {
+    id: "4",
+    clientName: "David",
+    orderNumber: 104,
+    startDate: new Date("2023-04-01"),
+    endDate: new Date("2023-04-10"),
+    isFinished: true,
+    size: 2,
+  },
+  {
+    id: "5",
+    clientName: "Eva",
+    orderNumber: 105,
+    startDate: new Date("2023-05-01"),
+    endDate: new Date("2023-05-10"),
+    isFinished: false,
+    size: 6,
+  },
+  {
+    id: "6",
+    clientName: "Frank",
+    orderNumber: 106,
+    startDate: new Date("2023-06-01"),
+    endDate: new Date("2023-06-10"),
+    isFinished: true,
+    size: 3,
+  },
+  {
+    id: "7",
+    clientName: "Grace",
+    orderNumber: 107,
+    startDate: new Date("2023-07-01"),
+    endDate: new Date("2023-07-10"),
+    isFinished: false,
+    size: 5,
+  },
+  {
+    id: "8",
+    clientName: "Henry",
+    orderNumber: 108,
+    startDate: new Date("2023-08-01"),
+    endDate: new Date("2023-08-10"),
+    isFinished: true,
+    size: 4,
+  },
+  {
+    id: "9",
+    clientName: "Ivy",
+    orderNumber: 109,
+    startDate: new Date("2023-09-01"),
+    endDate: new Date("2023-09-10"),
+    isFinished: false,
+    size: 7,
+  },
+  {
+    id: "10",
+    clientName: "Jack",
+    orderNumber: 110,
+    startDate: new Date("2023-10-01"),
+    endDate: new Date("2023-10-10"),
+    isFinished: true,
+    size: 2,
+  },
+];
 
 function App() {
+  const [orders, setOrders] = useState<OrderType[]>([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <ul>{orders.map(order) => {
+        <li>
+          <div>
+            {order.orderNumber}
+            {order.clientName}
+          </div>
+        </li>
+      } }</ul> */}
     </div>
   );
 }
---
+++ src/OrdersList.tsx
@@ -0,0 +1,58 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { OrderType } from "./App";
+
+export type orderType = {
+  id: string;
+  clientName: string;
+  orderNumber: number;
+  startDate: Date;
+  endDate: Date;
+  isFinished: boolean;
+  size: number;
+};
+
+const OrdersList = ({ orders }: { orders: OrderType[] }) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Nr. Zlecenia : </TableCell>
+            <TableCell align="right">Nazwa klienta : </TableCell>
+            <TableCell align="right">metry2</TableCell>
+            <TableCell align="right">Przyjęte zlecenie: </TableCell>
+            <TableCell align="right">Data wydania : </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {orders.map((order) => (
+            <TableRow
+              key={order.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {order.orderNumber}
+              </TableCell>
+              <TableCell align="right">{order.clientName}</TableCell>
+              <TableCell align="right">{order.size}</TableCell>
+              <TableCell align="right">
+                {order.startDate.toDateString()}
+              </TableCell>
+              <TableCell align="right">
+                {order.endDate.toDateString()}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
