f654107b1685a99bfe514196726e879f4e7f2dbc
dawidj <dawidjanicki2@gmail.com>
2024-02-02 21:43:02 +0100

    Firebase setup

---
+++ src/FireBaseCfg.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBUJa6UY2zt3nv5CbmgqiGacfokXJIjXo0",
+    authDomain: "primo-bed4e.firebaseapp.com",
+    projectId: "primo-bed4e",
+    storageBucket: "primo-bed4e.appspot.com",
+    messagingSenderId: "818458840657",
+    appId: "1:818458840657:web:a2c54b37b50f070171f270"
+  };
+
+  export default firebaseConfig;
